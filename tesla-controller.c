d913e4351b2a79df64e9da0e4c958bcd6f25190b
hotchk155 <jason_hotchkiss@hotmail.com>
2018-09-14 13:28:48 +0100

    work

--- tesla-controller.c
+++ tesla-controller.c
@@ -69,6 +69,7 @@ enum {
 };
 
 typedef struct {
+	byte midi_note;
 	int freq;
 	int duty;
 	
@@ -83,6 +84,15 @@ typedef struct {
 } OSCILLATOR;
 
 OSCILLATOR osc1;
+OSCILLATOR osc2;
+
+	enum {
+		MODE_OFF,
+		MODE_MIDI_MONO,
+		MODE_MIDI_DUAL,
+		MODE_PITCH,
+		MODE_PITCHLO
+	};
 
 
 #define NUM_ADC_CHANNELS	4
@@ -150,6 +160,42 @@ void interrupt( void )
 		pir1.2 = 0;
 	}
 
+	// Tone generation for voice 2
+	if(pir2.0) //CCP2IF
+	{	
+		if(osc2.phase == WAVE_LOW) { // AT END OF LOW PHASE
+			if(osc2.high_ticks) { // nonzero duty?			
+				P_OUT2 = 1;
+				osc2.next_event += osc2.high_ticks;
+				osc2.phase = WAVE_HIGH;		
+			}
+			else {
+				P_OUT2 = 0;
+				osc2.next_event += osc2.low_ticks;
+			}
+		}
+		else { // AT END OF HIGH PHASE
+			if(osc2.low_ticks) { // nonzero duty?			
+				P_OUT2 = 0;
+				osc2.next_event += osc2.low_ticks;
+				osc2.phase = WAVE_LOW;		
+			}
+			else {
+				P_OUT2 = 1;
+				osc2.next_event += osc2.high_ticks;
+			}
+		}
+		ccpr2h = osc2.next_event >> 8;
+		ccpr2l = (byte)osc2.next_event;
+		
+		if(osc2.update) { // got new frequency/duty info?
+			osc2.high_ticks = osc2.next_high_ticks;
+			osc2.low_ticks = osc2.next_low_ticks;
+			osc2.update = 0;
+		}
+		
+		pir2.0 = 0;
+	}
 		
 	// serial rx ISR
 	if(pir1.5)
@@ -184,12 +230,14 @@ void osc_duty(OSCILLATOR *posc, int duty) {
 	posc->duty = duty;
 	osc_apply(posc);
 }
-void osc_play(OSCILLATOR *posc, int freq) {
+void osc_play(OSCILLATOR *posc, int freq, byte midi_note) {
+	posc->midi_note = midi_note;
 	posc->freq = freq;
 	osc_apply(posc);
 }
 void osc_stop(OSCILLATOR *posc) {
 	while(posc->update);
+	posc->midi_note = NO_NOTE;
 	posc->next_high_ticks = 0;
 	posc->next_low_ticks = 1000;
 	posc->update = 1;
@@ -222,6 +270,8 @@ ADC_CHAN adc_chan[NUM_ADC_CHANNELS] = {
 };
 byte adc_current = 0xFF;
 byte adc_delay = 0; // counter used to force delay for cap to charge
+
+int pitch_bend = 0;
 
 ///////////////////////////////////////////////////////
 // ADC STATE MACHINE
@@ -400,73 +450,7 @@ byte midi_in()
 	return 0;
 }
 
-
-#define MAX_NOTES 8
-int note_count = 0;
-byte note_held[MAX_NOTES];
-
-static byte add_note(byte note) {
-	char i;
-
-	// if the note is already at top of stack
-	// there is nothing to do
-	if(note_count>0 && note == note_held) {
-		return 0;
-	}
-	
-	// check if note is already in the stack
-	for(i = 0; i < note_count; ++i) {
-		if(note_held[i] == note) { // found it
-			// shuffle all lower indexed notes up one place
-			for(; i > 0; --i) {
-				note_held[i] = note_held[i-1];
-			}
-			// and place this note at the front
-			note_held[0] = note;
-			return 1;
-		}
-	}
-
-	// is the note stack full?
-	if(note_count == MAX_NOTES) { 
-		// ok, we're going to lose the oldest note
-		for(i = MAX_NOTES-1; i > 0; --i) {
-			note_held[i] = note_held[i-1];
-		}
-	}
-	else {
-		// otherwise make space for the new note
-		for(i = note_count; i > 0; --i) {
-			note_held[i] = note_held[i-1];
-		}
-		++note_count;
-	}
-	// add the new note
-	note_held[0] = note;	
-	return 1;
-}
-
-///////////////////////////////////////////////////////////////
-// REMOVE A NOTE FROM A STACK
-static byte remove_note(byte note) 
-{
-	char i;
-	
-	// search for the note
-	for(i = 0; i < note_count; ++i) {
-		if(note_held[i] == note) { 
-			// remove the note by shufflng all later notes down
-			--note_count;
-			for(; i<note_count; ++i) {
-				note_held[i] = note_held[i+1];
-			}
-			return 1;
-		}
-	}
-	return 0;
-}
-
-int get_freq(byte note) {
+int get_freq(long note, int bend) {
 
 	const int f[] = {	
 		4186,	//c
@@ -485,39 +469,179 @@ int get_freq(byte note) {
 	while(note > 119) {
 		note -=12;
 	}
-	return  f[note%12]>>(9-(note/12));	
+	
+	note <<= 8;
+	note += bend;
+	bend = note & 0xFF;
+	note >>= 8;
+
+	int hz = f[note%12];	
+	hz += (int)(((long)hz*244*bend)/0x100000L);
+	
+	return  hz>>(9-(note/12));	
+}
+
+static void play_note(OSCILLATOR *posc, byte note) {
+	//if(posc->midi_note == note) {
+//		return;
+//	}
+	//else 
+	if(note == NO_NOTE) {
+		osc_stop(posc);
+	}
+	else {
+		osc_play(posc, get_freq(note, pitch_bend), note);
+	}
+}
+
+
+
+#define MAX_NOTES 8
+typedef struct {
+	int count;
+	byte note[MAX_NOTES];
+} NOTE_STACK;
+NOTE_STACK notes_1;
+NOTE_STACK notes_2;
+
+///////////////////////////////////////////////////////////////
+// ADD A NOTE TO A STACK
+static byte add_note(NOTE_STACK *pstack, byte note) {
+	char i;
+
+	// if the note is already at top of stack
+	// there is nothing to do
+	if(pstack->count>0 && note == pstack->note[0]) {
+		return 0;
+	}
+	
+	// check if note is already in the stack
+	for(i = 0; i < pstack->count; ++i) {
+		if(pstack->note[i] == note) { // found it
+			// shuffle all lower indexed notes up one place
+			for(; i > 0; --i) {
+				pstack->note[i] = pstack->note[i-1];
+			}
+			// and place this note at the front
+			pstack->note[0] = note;
+			return 1;
+		}
+	}
+
+	// is the note stack full?
+	if(pstack->count == MAX_NOTES) { 
+		// ok, we're going to lose the oldest note
+		for(i = MAX_NOTES-1; i > 0; --i) {
+			pstack->note[i] = pstack->note[i-1];
+		}
+	}
+	else {
+		// otherwise make space for the new note
+		for(i = pstack->count; i > 0; --i) {
+			pstack->note[i] = pstack->note[i-1];
+		}
+		++pstack->count;
+	}
+	// add the new note
+	pstack->note[0] = note;	
+	return 1;
 }
 
 ///////////////////////////////////////////////////////////////
-// PRIORITIZE A NOTE
-static void note_msg(byte note, byte vel)
+// REMOVE A NOTE FROM A STACK
+static byte remove_note(NOTE_STACK *pstack, byte note) 
 {
+	char i;
 	
-	byte i = 0;
-	byte new_note = NO_NOTE;
-
-	// maintain stack of notes that are held
-	if(vel) {
-		add_note(note);
-	}
-	else {
-		remove_note(note);
-	}
-	
-	for(i=0; i<note_count; ++i) {				
-		if(new_note == NO_NOTE || new_note < note_held[i]) {
-			new_note = note_held[i];
+	// search for the note
+	for(i = 0; i < pstack->count; ++i) {
+		if(pstack->note[i] == note) { 
+			// remove the note by shufflng all later notes down
+			--pstack->count;
+			for(; i<pstack->count; ++i) {
+				pstack->note[i] = pstack->note[i+1];
+			}
+			return 1;
 		}
 	}
-	
-	if(new_note != NO_NOTE) {
-		osc_play(&osc1, get_freq(new_note));
-	}
-	else {
-		osc_stop(&osc1);
-	}	
+	return 0;
 }
 
+///////////////////////////////////////////////////////////////
+static void note_msg(NOTE_STACK *pstack, byte note, byte vel, byte mode)
+{
+	int i;
+	
+	// maintain stack of notes that are held
+	if(vel) {
+		add_note(pstack, note);
+	}
+	else {
+		remove_note(pstack, note);
+	}	
+		
+	byte note1 = NO_NOTE;
+	byte note2 = NO_NOTE;
+	for(int i=0; i<pstack->count; ++i) {
+	
+		// get the highest note
+		if(note1 == NO_NOTE || pstack->note[i] > note1) {
+			note2 = note1;
+			note1 = pstack->note[i];
+		}
+		else if(note2 == NO_NOTE || pstack->note[i] > note2) {
+			note2 = pstack->note[i];
+		}
+	}
+
+	if(mode == MODE_MIDI_DUAL) 
+	{
+		if(note1 == NO_NOTE) {
+			// kill both
+			play_note(&osc1, NO_NOTE);
+			play_note(&osc2, NO_NOTE);
+		}
+		else if(note2 == NO_NOTE) {
+			// play same note on both
+			play_note(&osc1, note1);
+			play_note(&osc2, note1);
+		}
+		else {
+			if(osc2.midi_note == note1) { // the remaining note is playing on osc2
+				play_note(&osc1, note2);	// note 2 plays on osc1 while remining note stays on osc2
+			}
+			else {
+				play_note(&osc1, note1);	// the notes assigned osc1, osc2
+				play_note(&osc2, note2);
+			}
+		}
+	}
+	else
+	{
+		play_note(&osc1, note1);
+		play_note(&osc2, note1);
+	}
+
+}
+
+static void cc_msg(byte cc, byte val) {
+	if(cc == 1) {
+		byte i=val * 2;
+		if(i < 5) val = 5;
+		if(i > 250) val = 250;
+		osc_duty(&osc1, i);		
+		osc_duty(&osc2, i);		
+	}
+}
+
+static void bend_msg(byte msb, byte lsb) {
+	long bend = ((int)msb<<7|(lsb&0x7F)) - 8192;		// -8192 thru + 8192	
+	bend *= 256; // bend is store as MIDI notes * 256
+	bend /= 682; // over approx 1 octave 
+	pitch_bend = bend;
+	play_note(&osc1, osc1.midi_note);
+	play_note(&osc2, osc2.midi_note);	
+}
 
 ////////////////////////////////////////////////////////////
 // MAIN
@@ -571,22 +695,28 @@ void main()
 
 
 	
-/*
-	clock at 4MHz.. 1/4 prescale, 1MHz resolution
-	
-	440Hz
-	full cycle = 0.0022727272727273 sec
-	= 1136 counts per wave cycle
-	= 568 counts per flip at 50% duty
-*/
+//
+//	clock at 4MHz.. 1/4 prescale, 1MHz resolution
+//	
+//	440Hz
+//	full cycle = 0.0022727272727273 sec
+//	= 1136 counts per wave cycle
+//	= 568 counts per flip at 50% duty
+
 	osc_init(&osc1);
+	osc_init(&osc2);
 	
 //	set_osc1(356,128);
 	ccp1con = 0b00001010; // software interrupt only
+	ccp2con = 0b00001010; // software interrupt only
 	
 	ccpr1h = 0;
 	ccpr1l = 0;
 	pie1.2 = 1; // CCP1IE
+		
+	ccpr2h = 0;
+	ccpr2l = 0;
+	pie2.0 = 1; // CCP2IE
 	
 
 	// Configure timer 1
@@ -598,16 +728,14 @@ void main()
     adcon1=0b11010000; // right justify, Fosc/16, Vdd - Vss
     adcon0=0b00000001; // turn on the ADC
 
-	enum {
-		MODE_OFF,
-		MODE_MIDI,
-		MODE_PITCH,
-		MODE_PITCHLO
-	};
 	byte new_mode = MODE_OFF;
 	byte mode = MODE_OFF;
+	adc_chan[KNOB_MODE].status = 1;
+	
+	notes_1.count = 0;
+	notes_2.count = 0;
 	for(;;) {
-	P_LED = 0;
+P_LED = (notes_1.count > 2);
 		adc_run();
 		
 		if(adc_chan[KNOB_MODE].status) {
@@ -616,67 +744,77 @@ void main()
 				new_mode = MODE_OFF;
 			}
 			else if(adc_chan[KNOB_MODE].result < 300) {
-				new_mode = MODE_MIDI;
+				new_mode = MODE_MIDI_MONO;
 			}
 			else if(adc_chan[KNOB_MODE].result < 500) {			
+				new_mode = MODE_MIDI_DUAL;
+			}
+			else if(adc_chan[KNOB_MODE].result < 700) {
+				new_mode = MODE_MIDI_DUAL;
+			}
+			else if(adc_chan[KNOB_MODE].result < 900) {
 				new_mode = MODE_PITCH;
 			}
-			else {//if(adc_chan[KNOB_MODE].result < 700) {
-				new_mode = MODE_PITCHLO;
-			}/*
-			else if(adc_chan[KNOB_MODE].result < 900) {
-			}
 			else {
-			}*/
+				new_mode = MODE_PITCHLO;
+			}
 
 			if(new_mode != mode) {
 				switch(new_mode) {
 					case MODE_OFF:
 						osc_stop(&osc1);
+						osc_stop(&osc2);
 						break;
-					case MODE_MIDI:					
+					case MODE_MIDI_MONO:					
+					case MODE_MIDI_DUAL:					
 						osc_stop(&osc1);
+						osc_stop(&osc2);
+						notes_1.count = 0;
+						notes_2.count = 0;
 						rx_head = 0;
 						rx_tail = 0;
+						pitch_bend = 0;
 						break;
 					case MODE_PITCH:
-						osc_play(&osc1, adc_chan[KNOB_MODE].result);
+						osc_play(&osc1, adc_chan[KNOB_MODE].result, 0);
+						osc_play(&osc2, adc_chan[KNOB_MODE].result, 0);
 						break;
 					case MODE_PITCHLO:
-						osc_play(&osc1, adc_chan[KNOB_MODE].result/16);
+						osc_play(&osc1, adc_chan[KNOB_MODE].result/16, 0);
+						osc_play(&osc2, adc_chan[KNOB_MODE].result/16, 0);
 						break;
 				}
 				mode = new_mode;
 			}
 		}
 		
+		byte msg = midi_in();		
 		switch(mode) {
-		case MODE_MIDI:	
-			{
-				byte msg = midi_in();		
-				switch(msg & 0xF0) {
-				// MIDI NOTE OFF
-				case 0x80:
-				case 0x90:
-					note_msg(midi_params[0], midi_params[1]);
-					P_LED = 1;
-					break;
-				default:
-					P_LED = 0;
-					break;
-				}		
-			}
+		case MODE_MIDI_MONO:	
+		case MODE_MIDI_DUAL:	
+			switch(msg) {
+			// MIDI NOTE 
+			case 0x80: note_msg(&notes_1, midi_params[0], 0, mode); break;
+			case 0x90: note_msg(&notes_1, midi_params[0], midi_params[1], mode); break;
+			case 0xB0: cc_msg(midi_params[0], midi_params[1]); break;
+			case 0xE0: bend_msg(midi_params[1], midi_params[0]); break;
+			default:
+				//P_LED = 0;
+				break;
+			}		
 			break;
 		case MODE_PITCH:	
 			if(adc_chan[KNOB_FREQ].status) {
 				adc_chan[KNOB_FREQ].status = 0;
-				osc_play(&osc1, adc_chan[KNOB_FREQ].result);
+				osc_play(&osc1, adc_chan[KNOB_FREQ].result, 0);
+				osc_play(&osc2, adc_chan[KNOB_FREQ].result, 0);
 			}
 			break;
 		case MODE_PITCHLO:	
 			if(adc_chan[KNOB_FREQ].status) {
 				adc_chan[KNOB_FREQ].status = 0;
-				osc_play(&osc1, adc_chan[KNOB_FREQ].result/16);
+				osc_play(&osc1, adc_chan[KNOB_FREQ].result/16, 0);
+				osc_play(&osc2, adc_chan[KNOB_FREQ].result/16, 0);
 			}
 			break;
 		}
@@ -693,6 +831,20 @@ void main()
 				osc_duty(&osc1, adc_chan[KNOB_DUTY1].result>>2);
 			}
 		}
+		
+		if(adc_chan[KNOB_DUTY2].status) {
+			adc_chan[KNOB_DUTY2].status = 0;
+			if(adc_chan[KNOB_DUTY2].result < 2) {
+				osc_stop(&osc2);
+			}
+			else if(adc_chan[KNOB_DUTY1].result > 1020) {
+				osc_duty(&osc2, 255);
+			}
+			else {
+				osc_duty(&osc2, adc_chan[KNOB_DUTY2].result>>2);
+			}
+		}
+		
 	}
 }
 
